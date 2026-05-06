a55a1117089bd889c23846a919b50d90294789e2
bcw222 <bcw0613@163.com>
2026-05-06 08:52:09 +0800

    Added envvar print on start.

--- node.mjs
+++ node.mjs
@@ -4,6 +4,10 @@ import worker from "./src/worker.mjs";
 
 const port = +(process.env.PORT || 8080);
 
+console.log("GEMINI_BASE_URL:", process.env.GEMINI_BASE_URL || "(default)");
+console.log("PORT:", port);
+console.log("NODE_ENV:", process.env.NODE_ENV || "(unset)");
+
 const serverAdapter = createServerAdapter(worker.fetch);
 const server = createServer(serverAdapter);
 server.listen(port, () => {
